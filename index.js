69adb240be3154dea560b358c3572157cdf1375a
yuqi-bi <yuqi.bi@rea-group.com>
2019-09-02 17:58:22 +0800

    refactor: refactor check logic

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const chalk = require('chalk')
 const portfinder = require('portfinder')
 
 const webpackAutoFindPort = async ({ config, logger }) => {
-  const oldPort = config.devServer.port
-  if (!oldPort) {
-    console.log(chalk.red('Please provide a standed webpack devServer config.'))
-    process.exit()
+  let defaultPort = 8080
+  if (config.devServer && config.devServer.port) {
+    defaultPort = config.devServer.port
+  } else {
+    config.devServer = {}
+    config.devServer.port = defaultPort
   }
 
-  portfinder.basePort = oldPort
+  if (!config.devServer) {
+    config.devServer = {}
+    config.devServer.port = defaultPort
+  }
+
+  if (config.devServer && !config.devServer.port) {
+    config.devServer.port = defaultPort
+  }
+
+  portfinder.basePort = defaultPort
 
   portfinder.getPort({ host: '127.0.0.1' }, (err, port) => {
     if (err) {
-      console.log(chalk.red('Can not find a useable port, please check yourself.'))
+      console.log(chalk.red('Can not find a useable port, please check your local port.'))
       return
     }
 
